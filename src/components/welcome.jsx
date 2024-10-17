6ff69f6d9d798958971e15fa029879dd3a24acdd
Matheus Bertemes <mathbertemes@gmail.com>
2024-10-16 18:06:03 -0700

    welcome component

---
+++ src/components/welcome.jsx
@@ -0,0 +1,17 @@
+
+
+const Welcome = ({
+  title,
+  description
+}) => {
+  return(
+    <div className="h-[90vh] max-w-7xl mx-auto">
+      <div>
+        <h1 className="text-7xl font-semibold my-11">{title}</h1>
+        <p>{description}</p>
+      </div>
+    </div>
+  )
+}
+
+export default Welcome;
